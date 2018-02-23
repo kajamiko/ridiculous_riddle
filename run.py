62554fbf0ba15d32e0f733c9ad30a8b7a9aa84ed
Kaja Mikolajczyk <kajaths@gmail.com>
2018-02-23 21:59:57 +0000

    Adding user and redirecting to the game's instance page

--- run.py
+++ run.py
@@ -1,14 +1,20 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return "Hello! I'm working! Yay!"
+@app.route('/', methods=["GET", "POST"])
+def index():
+    """Welcome page with a form to send username and start game"""
+    if request.method == "POST":
+        return redirect(request.form["username"])
+    return render_template('index.html')
     
-    
+@app.route('/<username>')
+def game(username):
+    """The instance game page, where riddles and score will be displayed  """
+    return render_template('game.html', username= username)
 
 app.run(host=os.getenv('IP'), port=(int(os.getenv('PORT'))), debug=True)
