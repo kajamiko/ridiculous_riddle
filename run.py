e6b62abf977ddb00afdf2edbbaf49a8e7a12b2ed
Kaja Mikolajczyk <kajaths@gmail.com>
2018-02-27 16:31:30 +0000

    added redirecting to the game page

--- run.py
+++ run.py
@@ -16,32 +16,38 @@ def index():
 @app.route('/<username>', methods=["GET","POST"])
 def user(username):
     # user starting page, with an encouraging image and a simple form with start button
-    #score = 0
+    level=1
  
     if request.method == "POST":
-        return redirect(username)
+        return redirect("/"+username+"/"+str(level))
     return render_template('user.html', username = username)
 
 @app.route("/<username>/<level>", methods=["GET","POST"])
 def game(username, level):
     """ The instance game page, where riddles and score will be displayed  """
-    # Opening file with our riddles
-    riddle_text = ""
+
+    """ After clicking submit, the json file is being checked for riddle with specific number, then  
+    """
+    rlist = []
     with open("data/riddles.json", "r") as json_data:
         riddle_data = json.load(json_data)
         for obj in riddle_data:
             if obj["level"] == level:
-                riddle_text = obj["riddle"]
-                """
-                if obj["answer"] == request.form["answer"]:
-                    return redirect('game.html', username = username, level=level+1)
-                else:
-                    flash("<h2>Oops! Wrong! The answer is not<br>{}</h2>".format(request.form["answer"]))
-                """
-            
+                rlist.append(obj["riddle"])
+                rlist.append(obj["answer"])
+                rlist.append(obj["img_source"])
+        if request.method == "POST":
+            if rlist[1].lower() == request.form["answer"].lower():
+                return render_template("game.html",
+                username=username,
+                level=level+1)
+            else:
+                flash("<h2>Oops! Wrong! The answer is not<br>{}</h2>".format(request.form["answer"]))
+                
     return render_template('game.html',
-    riddle_text = riddle_text,
-    username = username,
-    level=level,)       
+    riddle_text = rlist[0],
+    riddle_image= rlist[2],
+    username=username,
+    level=level)       
 
 app.run(host=os.getenv('IP'), port=(int(os.getenv('PORT'))), debug=True)
