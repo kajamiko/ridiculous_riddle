298388d3ca0210b37fc3e9f51ba8a233531bcc13
Kaja Mikolajczyk <kajaths@gmail.com>
2018-07-17 08:38:48 +0000

    Project working but needs some content and a deep front-end rebuild

--- run.py
+++ run.py
@@ -6,48 +6,68 @@ from flask import Flask, render_template, request, redirect, flash, url_for
 app = Flask(__name__)
 app.secret_key = "some_secrets"
 
+
+
 @app.route('/', methods=["GET", "POST"])
 def index():
     """Welcome page with a form to send username and start game"""
     if request.method == "POST":
+        with open("data/users.txt", "a") as file:
+            file.write(request.form["username"], "\n")
         return redirect(request.form["username"])
     return render_template('index.html')
     
 @app.route('/<username>', methods=["GET","POST"])
-def user(username):
+def user(username, game_over=False):
     # user starting page, with an encouraging image and a simple form with start button
     
     if request.method == "POST":
         level="1"
         return redirect(url_for('game', username=username, level=level))
-       
+    if game_over :
+        """
+        
+        """
+        with open("data/users.txt", "r") as file:
+            data = file.readlines()
+            for line in data:
+                words = line.split()
+        return render_template('game_over', username=username, words=words)
     return render_template('user.html', username = username)
+    
+
 
 
 @app.route("/game/<username>/<level>", methods=["GET","POST"])
 def game(username, level):
-    """ This function creates instance game page, where riddles and score are displayed.
+    """ This function creates instance game page, where riddles are displayed.
     After clicking submit, the json file is being checked for riddle with specific number stored as level, then
-    it the data are copied to a 3-element table.
+    it the data are copied to a 3-element list.
     """
     rlist = []
     with open("data/riddles.json", "r") as json_data:
         riddle_data = json.load(json_data)
         for obj in riddle_data:
             if obj["level"] == level:
+                """
+                Finds proper riddle
+                """
                 rlist.append(obj["riddle"])
                 rlist.append(obj["answer"])
                 rlist.append(obj["img_source"])
         if request.method == "POST":
             if rlist[1].lower() == request.form["answer"].lower():
-                """If the level value is still smaller or same as the number of riddle objects - if there are still 
-                any riddles to answer - then we get another view with the next riddle. Otherwise, gets back to user view with leaderboard"""
+                """
+                When the answer is correct...
+                """
                 if int(level) <= len(riddle_data):
+                    """If the level value is still smaller or same as the number of riddle objects - if there are still 
+                    any riddles to answer - then we get another view with the next riddle. Otherwise, gets back to user view with leaderboard"""
                     
                     new_level = str(int(level) + 1)
                     return redirect(url_for('game', username=username, level=new_level))
                 else:
-                    return render_template('user.html', username = username)
+                    return render_template('user.html', username = username, game_over=True)
             else:
                 flash("Oops! Wrong! The answer is not {}".format(request.form["answer"]))
                 
